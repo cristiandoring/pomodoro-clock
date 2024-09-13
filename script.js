2144d07f3952025017ec5daf5697a4eb2c4faf31
cristiandoring <cristiandoring@gmail.com>
2024-09-13 17:49:01 -0300

    Botão iniciar cronômetro

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var segundos = 0;
-var minutos = 0;
+var minutos = 50;
 var horas = 0;
 
 /*
@@ -13,22 +13,32 @@ function iniciar(){
     //chama a função contador a cada 1s (1000ms)
      intervalo = setInterval(cronometro,1000)
 
-     document.querySelector('.iniciar').disabled = true;
-
      //desabilita o botão iniciar após clicar 1x nele
+     document.querySelector('#iniciar').disabled = true;
+
+     
 }
 
 
 function cronometro(){
-    segundos++
-    if(segundos == 60){
-        minutos++
-        segundos=0
-    }
-
-    if(minutos == 60){
-        horas++
-        minutos=0
+    if (segundos == 0) {
+        if (minutos == 0) {
+            if (horas == 0) {
+                // Para o cronômetro quando chegar a zero
+                clearInterval(intervalo);
+                alert("Tempo esgotado!");
+                return;
+            } else {
+                horas--;
+                minutos = 59;
+                segundos = 59;
+            }
+        } else {
+            minutos--;
+            segundos = 59;
+        }
+    } else {
+        segundos--;
     }
     
     document.getElementById('relogio').innerText = formatar(horas) + ":" + formatar(minutos) + ":" + formatar(segundos)
